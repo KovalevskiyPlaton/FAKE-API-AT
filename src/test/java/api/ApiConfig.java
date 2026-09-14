8c64c41faf14c3f9b7a4a6decbbef485a105fc40
KovalevskiyPlaton <soulshon@mail.ru>
2026-09-14 08:46:59 +0300

    settings files

--- src/test/java/api/ApiConfig.java
+++
@@ -1,6 +0,0 @@
-package api;
-
-public class ApiConfig {
-    public static final String BASE_URL = "https://reqres.in/";
-    public static final String API_KEY = System.getProperty("api.key", "API in here!");
-}
