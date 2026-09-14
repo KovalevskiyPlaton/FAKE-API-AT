7cb26b1476fa011d2aa2584f30a1dde8d3e1c625
Platon <soulshon@mail.ru>
2026-09-14 22:17:50 +0300

    added second part files

--- src/test/java/tests/ReqresTest.java
+++ src/test/java/tests/ReqresTest.java
@@ -1,11 +1,15 @@
 package tests;
 
+import api.protect.Specifications;
+import groovyjarjarpicocli.CommandLine;
 import io.restassured.http.ContentType;
 import api.model.UserData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.ApiConfig;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
@@ -14,15 +18,27 @@ public class ReqresTest {
 
     @Test
     public void checkAvatarAndIdtest(){
-
+        Specifications.installSpecification(Specifications.reqestSpec(URL), Specifications.responseSpecOk200());
         List<UserData> users = given()
                 .queryParam("key", ApiConfig.API_KEY)
                 .when()
                 .log().all()
-                .contentType(ContentType.JSON)
-                .get(URL+ "api/users?page=2")
+                .get("api/users?page=2")
                 .then().log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
-        int i = 0;
+
+        /*Проверка: id пользователя  встречается в аватаре пользователя */
+        users.stream().forEach(x-> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
+
+        /*Проверка: e-mail каждого пользователя заканчивается на '@reqres.in' */
+        Assert.assertTrue(users.stream().allMatch(x->x.getEmail().endsWith("@reqres.in")));
+
+        /*Проверка: id пользователя  встречается в аватаре пользователя  - Вариант 2 (через коллекции)*/
+        List<String> avatars = users.stream().map(UserData::getAvatar).collect(Collectors.toList());
+        List<String> ids = users.stream().map(x-> x.getId().toString()).collect(Collectors.toList());
+
+        for(int i =0; i<avatars.size(); i++){
+            Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
+        }
     }
 }
